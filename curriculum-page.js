62e3dcc5755635d9f502e27d9dab918194a20e7e
Adominguezes <alberto@adominguez.es>
2017-06-04 16:17:56 +0200

    feat(*):  initial commit

---
+++ curriculum-page.js
@@ -0,0 +1,36 @@
+(function () {
+  'use strict';
+  Polymer({
+
+    is: 'curriculum-page',
+
+    properties: {
+      selected: {
+        type: Number,
+        value: 0,
+        observer: '_changeSelected'
+      },
+      data: {
+        type: Array,
+        observer: '_setDataContent'
+      },
+      dataContent: {
+        type: Array,
+        value: function() {
+          return [];
+        }
+      }
+    },
+
+    _changeSelected: function() {
+      this._setDataContent();
+    },
+
+    _setDataContent: function() {
+      if(this.data) {
+        this.dataContent = this.data[this.selected].content;
+      }
+    }
+
+  });
+}());
